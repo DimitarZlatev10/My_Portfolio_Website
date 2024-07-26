2c4efcb151c30e5203ab18c8a98af25da1f804f4
Dimitar Zlatev <dimitarzlatev02@gmail.com>
2024-07-26 12:25:36 +0300

    installed nuxt and tailwind.css

---
+++ layouts/navigation-bar.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+      <p>nav-bar</p>
+      <slot />
+    </div>
+  </template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'navigation-bar'
+})
+</script>
+
+
+<template>
+    <div>
+      <h1 class="text-red-500">Welcome to the homepage</h1>
+      <AppAlert>
+        This is an auto-imported component
+      </AppAlert>
+    </div>
+  </template>
