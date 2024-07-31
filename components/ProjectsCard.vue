cdd1385cac177f32786403ba8a516a18ea4150ce
Dimitar Zlatev <dimitarzlatev02@gmail.com>
2024-07-31 16:53:00 +0300

    created projects page and component

---
+++ components/ProjectsCard.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+const props = defineProps({
+    projects: {
+        type: Object,
+        required: true,
+    },
+
+});
+
+const { projects } = props;
+</script>
+
+<template>
+    <div class="bg-black rounded-lg shadow-md overflow-hidden" v-for="project in projects">
+        <NuxtLink :to="project.url" target="_blank">
+            <div class="overflow-hidden relative">
+                <img class="transition-transform duration-300 ease-in-out transform hover:scale-110 w-full"
+                    :src="project.image.imagePath" :alt="project.image.alt">
+            </div>
+        </NuxtLink>
+        <div class="p-6 bg-black rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
+            <NuxtLink :to="project.url" target="_blank" class="block mb-2">
+                <h2
+                    class="text-2xl sm:text-3xl font-bold text-white hover:text-gray-300 transition-colors duration-300">
+                    {{ project.name }}
+                </h2>
+            </NuxtLink>
+            <p class="text-base sm:text-lg text-gray-300 mt-2">
+                {{ project.description }}
+            </p>
+            <div class="flex flex-wrap items-center space-x-2 mt-4">
+                <p class="text-sm sm:text-base font-semibold text-gray-400">Skills Used:</p>
+                <p class="text-xs sm:text-sm text-gray-200 bg-gray-700 rounded-full px-2 py-1"
+                    v-for="skill in project.skillsUsed" :key="skill">
+                    {{ skill }}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
---
+++ pages/projects.vue
@@ -0,0 +1,87 @@
+<script setup lang="ts">
+useHead({
+    title: 'Projects',
+    meta: [
+        { name: 'description', content: 'Projects Page of my Portfolio Website.' }
+    ]
+})
+
+const projects = [
+    {
+        image: {
+            imagePath: '/images/AlexaCleaning.png',
+            alt: "Alexa Cleaning Image"
+        },
+        url: "https://alexacleaning.bg/",
+        name: "Alexa Cleaning",
+        description: "Worked on the front-end and back-end for this site. Created the booking form from scratch and implemented the admin panel with Filament PHP.",
+        skillsUsed: [
+            "Laravel",
+            "Livewire",
+            "Tailwind CSS",
+            "Filament PHP",
+            "MySQL",
+            "Alpine.js"
+        ]
+    },
+    {
+        image: {
+            imagePath: '/images/KVMExtreme.png',
+            alt: "KVM Extreme Image"
+        },
+        name: "KVM Extreme",
+        url: "https://kvmextreme.bg/",
+        description: "Worked mostly on the back-end for this site. Implemented the affiliates program for this site.",
+        skillsUsed: [
+            "Laravel",
+            "Livewire",
+            "Tailwind CSS",
+            "Filament PHP",
+            "MySQL",
+            "Alpine.js"
+        ]
+    },
+    {
+        image: {
+            imagePath: '/images/BilkoviTinkturi.png',
+            alt: "Bilkovi Tinkturi Image"
+        },
+        name: "Bilkovi Tinkturi",
+        url: "https://bilkovitinkturi.bg/",
+        description: "Integrated the Speedy API for this site.",
+        skillsUsed: [
+            "Nuxt.js",
+            "Tailwind CSS",
+            "GraphQL"
+        ]
+    },
+    {
+        image: {
+            imagePath: '/images/DataBridge Commerce.png',
+            alt: "DataBridge Commerce Image"
+        },
+        name: "DataBridge Commerce",
+        url: "https://apps.shopify.com/content-exporter",
+        description: "Built a Shopify App which uses the Shopify API to get the current Store data (products, collections, pages, blogs, etc.) so it can be used on another Store (duplicate Stores).",
+        skillsUsed: [
+            "Shopify",
+            "React.js",
+            "Express.js",
+            "JavaScript",
+            "GraphQL",
+        ]
+    },
+]
+
+</script>
+
+<template>
+    <div class="bg-gradient-to-r from-blue-500 via-pink-500 to-purple-500 min-h-screen">
+        <div class="container mx-auto p-4 animate-fade-in animation-delay-1">
+            <h1 class="text-4xl font-bold text-center mb-8">My Projects</h1>
+            <div class="grid grid-cols-1 sm:grid-cols-2 gap-8 mx-auto">
+                <ProjectsCard :projects="projects"></ProjectsCard>
+            </div>
+        </div>
+    </div>
+</template>
