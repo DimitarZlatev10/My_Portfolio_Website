8fcecef55a125d6a4da9ea137a06d58a58c836aa
Dimitar Zlatev <dimitarzlatev02@gmail.com>
2024-07-31 16:52:34 +0300

    updated experience page and created component

---
+++ components/ExperienceCard.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+const props = defineProps({
+    jobs: {
+        type: Object,
+        required: true,
+    },
+
+});
+
+const { jobs } = props;
+</script>
+
+<template>
+    <div v-for="(job, index) in jobs" :key="index"
+        class="bg-white shadow-lg rounded-lg p-8 transition-transform transform">
+        <div class="flex items-center justify-between mb-4">
+            <NuxtLink class="text-2xl font-semibold text-blue-600 hover:text-blue-400" target="_blank" :to="job.url">
+                <h2>{{ job.companyName }}</h2>
+            </NuxtLink>
+
+            <span class="text-sm text-gray-500">{{ job.period }}</span>
+        </div>
+        <p class="text-lg text-gray-700 mb-6">Position: {{ job.position }}</p>
+        <div>
+            <h3 class="text-lg font-medium text-gray-700 mb-2">Acquired Skills:</h3>
+            <ul class="flex flex-wrap">
+                <li v-for="(skill, skillIndex) in job.skills" :key="skillIndex"
+                    class="bg-blue-600 text-white py-1 px-3 rounded-full text-sm mr-2 mb-2">
+                    {{ skill }}
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
--- pages/experience.vue
+++ pages/experience.vue
@@ -1,5 +1,54 @@
+<script setup>
+useHead({
+    title: 'Experience',
+    meta: [
+        { name: 'description', content: 'Experience Page of my Portfolio Website.' }
+    ]
+})
+
+const jobs = [
+    {
+        companyName: '411 Locals',
+        url: "https://411locals.com/",
+        position: 'Browser Automation Studio Specialist with JavaScript',
+        period: 'Jun 2023 - Oct 2023 · 5 mos',
+        skills: [
+            'Building Automation Systems (BAS)',
+            'Communication',
+            'Problem Solving',
+            'JavaScript',
+            'Teamwork'
+        ]
+    },
+    {
+        companyName: 'Valdo 69',
+        url: "https://papagal.bg/eik/204514887/2c9a#google_vignette",
+        position: 'Web Developer',
+        period: 'Nov 2023 - Present · 9 mos',
+        skills: [
+            'Laravel',
+            'Livewire',
+            'Tailwind CSS',
+            'Nuxt.js',
+            'Shopify',
+            'Filament PHP',
+            'Alpine.js',
+            "LangChain",
+            "Git",
+            "GraphQL",
+            "MySQL"
+        ]
+    }
+]
+</script>
+
 <template>
-    <section>
-        <p>Experience</p>
-    </section>
+    <div class="bg-gradient-to-r from-blue-500 via-pink-500 to-purple-500 min-h-screen p-6">
+        <div class="max-w-4xl mx-auto animate-fade-in animation-delay-1">
+            <h1 class="text-4xl font-bold text-center text-white mb-8">My Experience</h1>
+            <div class="space-y-12">
+                <ExperienceCard :jobs="jobs"></ExperienceCard>
+            </div>
+        </div>
+    </div>
 </template>
