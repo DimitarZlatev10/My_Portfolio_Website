481215e12c588e52b2a029d486ce29924742a607
Dimitar Zlatev <dimitarzlatev02@gmail.com>
2024-08-01 17:40:41 +0300

    added sitemap and seo updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,47 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      title: "Dimitar Zlatev",
+      meta: [
+        {
+          name: "description",
+          content: "Showcase of my work in web development.",
+        },
+        {
+          name: "keywords",
+          content: "portfolio, web development, UX/UI, frontend, backend",
+        },
+        { property: "og:title", content: "Dimitar Zlatev" },
+        {
+          property: "og:description",
+          content: "Explore my projects and skills in web development.",
+        },
+        {
+          property: "og:image",
+          content:
+            "https://master--dimitarzlatev.netlify.app//images/HomeImage.png",
+        },
+        {
+          property: "og:url",
+          content: "https://master--dimitarzlatev.netlify.app/",
+        },
+        { property: "og:type", content: "website" },
+      ],
+    },
+  },
+  sitemap: {
+    hostname: "https://master--dimitarzlatev.netlify.app",
+    routes: [
+      "/",
+      "/skills",
+      "/projects",
+      "/experience",
+      "/education",
+      "/about",
+      "/contact",
+    ],
+  },
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
